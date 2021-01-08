bab7bf68ffe39773329fb5b06d1b3b6e5580ca95
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-08 16:46:57 +0800

    Add navigation

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ContactList from "./src/screens/ContactList";
+import { Text, View } from 'react-native';
+import { ContactList, Home } from "./src/screens";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ContactList />
-      <StatusBar style="auto" />
-    </View>
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Contact List" component={ContactList} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
