9ac74d7d96c2b499c890babc18006fc317a70210
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-08 17:44:50 +0800

    Add an SOS button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -22,9 +22,12 @@ class Home extends React.Component {
       <View style={styles.container}>
         <Text>The Home Screen</Text>
         <TouchableOpacity onPress={this.goToAddContacts}>
-          <View style={styles.addContactsIcon}>
+          <View>
             <Ionicons name="ios-person-add" size={144} color="black" />
           </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.helpButton}>
+          <Text>SOS</Text>
         </TouchableOpacity>
       </View>
     )
@@ -33,12 +36,17 @@ class Home extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    'flex': 1,
-  },
-  addContactsIcon: {
-    flexDirection: 'column',
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center', 
+  },
+  helpButton: {
+    backgroundColor: "lightblue",
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: 'center',
   },
 }); 
 
