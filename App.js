c4a71213cba4f7c3da76590c02a203878f7792f2
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-08 11:47:25 +0800

    Add contact list to app

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ContactList from "./src/screens/ContactList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ContactList />
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -15,7 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
