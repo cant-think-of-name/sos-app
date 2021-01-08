1fe6b6ce4e124f88357e8d80379e922a4a5f0a66
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-08 17:34:01 +0800

    Add an icon to navigate to the contacts list

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { SafeAreaView, TouchableOpacity, View, StyleSheet, FlatList, Text } from 'react-native';
 
+import { Ionicons } from '@expo/vector-icons';
+
 // TODO: style this page
 // TODO: figure out how to add an SOS button
 class Home extends React.Component {
@@ -10,10 +12,20 @@ class Home extends React.Component {
     };
   }
 
+  goToAddContacts = () => {
+    const { navigation } = this.props;
+    navigation.navigate('Contact List');
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>The Home Screen</Text>
+        <TouchableOpacity onPress={this.goToAddContacts}>
+          <View style={styles.addContactsIcon}>
+            <Ionicons name="ios-person-add" size={144} color="black" />
+          </View>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -22,6 +34,11 @@ class Home extends React.Component {
 const styles = StyleSheet.create({
   container: {
     'flex': 1,
+  },
+  addContactsIcon: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center', 
   },
 }); 
 
